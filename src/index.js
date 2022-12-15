594df30de5dd3ad64b9da5bef55aacfb4a3ad275
labradorescence <orangenewyorkcreativeagency@gmail.com>
2022-12-15 11:47:38 -0500

    lab-review

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  //1. Select the form
+  const form = document.querySelector("#create-task-form")
+
+  //2. Grab the input text
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    //option 1
+    // const inputDesc = document.querySelector("#new-task-description").value
+
+    //option 2
+    const inputDesc = event.target.description.value
+    console.log(inputDesc)
+
+  //3. Select the task DOM obj
+    const tasks = document.querySelector("#tasks")
+    console.log(tasks)
+
+  //4. create li element for the new task
+     const newLi = document.createElement("li")
+
+  //5. insert the new todo inside of the li element
+    newLi.innerHTML = inputDesc
+    console.log(newLi)
+  //6. append the new li onto the task obj
+    tasks.appendChild(newLi)
+
+
+  ///////////DELETE////////////    
+  const deleteBtn = document.createElement('button')
+  deleteBtn.innerText = '❌'
+  newLi.append(deleteBtn)
+
+  deleteBtn.addEventListener("click", (e) => {
+    // const removingLi = e.target.parentNode
+    // console.log(removingLi)
+    // removingLi.remove()
+
+    newLi.remove()
+   })
+
+    
+  //7. reset
+  event.target.reset()
+
+  })
+
+
+
+
+
+
+
 });
